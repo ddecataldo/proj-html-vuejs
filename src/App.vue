6be180bc8da711a6ff28067a01c4c7a647ece0c1
ddecataldo <davidedecataldo92@gmail.com>
2021-11-22 18:18:15 +0100

    Menu e Slider

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <!-- NavBar -->
+      <nav>
+        <div class="container">
+          <div class="menu">
+            <div class="logo">
+              <img :src="require('@/assets/logo.png')" alt="Logo Avadatakeout">
+            </div>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Oder Online</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">News</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- Slider -->
+      <section class="hero">
+        <div class="container">
+          <p class="occhiello">Hungry?</p>
+          <h1><span>Great Food</span><br>Delivered</h1>
+          <p><a class="btn btn-red" href="#">view our menu</a></p>
+        </div>
+      </section>
+    </header>
+    <main>
+      
+    </main>
+    <section></section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "@/styles/app";
+
+/* Menu */
+nav{
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
 }
+.menu{
+  display: $d-flex;
+  justify-content: $justify-content-between;
+  align-items: $align-items-center;
+  padding: 70px 0;
+
+  ul{
+    list-style-type: none;
+  } 
+
+  ul li{
+    display: inline-block;
+    padding: 10px 15px;
+    font-size: 20px;
+  }
+
+  ul li a{
+    color: #fff;
+    display: block;
+    font-weight: 700;
+    padding-left:15px;
+    padding-right:15px;
+  }
+}
+
+/* Slider */
+.hero{
+  background-image: url('./assets/home-hero.jpg');
+  background-size: cover;
+  background-position: center center;
+  padding: 325px 0;
+  color: #fff;
+
+  .occhiello{
+    font-size: 40px;
+    margin-bottom: 12px;
+  }
+
+  h1{
+    font-size:80px;
+    line-height: 1em;
+    font-weight: bold;
+    font-style: normal;
+    margin-bottom: 35px;
+  }
+
+  span{
+    color: $primary;
+  }
+
+}
+
 </style>
